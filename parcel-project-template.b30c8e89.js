417e97ea7b3d02e24f3795f86d6b3cc6fb1caa25
frotrest <frotrest@users.noreply.github.com>
2025-11-14 10:59:15 +0000

    deploy: 150e2edc9c3917332c11538ffc7de5caa0ed72a2

--- parcel-project-template.b30c8e89.js
+++ parcel-project-template.b30c8e89.js
@@ -1,2 +1,2 @@
 let e=document.querySelector(".card");document.getElementById("colorBtn").addEventListener("click",()=>{e.classList.toggle("active")});
-//# sourceMappingURL=dudeshape.d1fdbb8e.js.map
+//# sourceMappingURL=parcel-project-template.b30c8e89.js.map
